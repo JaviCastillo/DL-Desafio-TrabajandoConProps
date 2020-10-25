6429694b8ad738e579f6cf23eee457958dc307d3
Javier <afro89@gmail.com>
2020-10-25 04:04:02 -0300

    First Commit

---
+++ src/Padre.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+    <Hijo :tareas="tareas" @enviarTarea="agregarTarea" @quitarTarea="eliminarTarea"/>
+  </div>
+</template>
+
+<script>
+import Hijo from './components/Hijo.vue'
+
+export default {
+  name: 'Padre',
+  components: {
+    Hijo
+  },
+  data() {
+    return {
+      tareas: []
+    }
+  },
+  methods: {
+    agregarTarea(task){
+      this.tareas.push(task);
+    },
+    eliminarTarea(i){
+      this.tareas.splice(i,1);
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Hijo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="centro">
+    <form @submit.prevent="enviarTarea(tarea)">
+      Tarea
+      <input type="text" placeholder="ingresa nueva tarea" v-model="tarea">
+      <button type="submit">Añade</button>
+    </form>
+    <ul class="lista">
+      <li v-for="(valor, index) in tareas" :key="index">
+        {{valor}} <button @click.prevent="quitarTarea(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hijo',
+  props:{
+    tareas:{
+      type: Array,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      tarea: ''
+    }
+  },
+  methods: {
+    enviarTarea(tarea){
+      if(tarea){
+        this.$emit('enviarTarea', tarea)
+        this.tarea = ''
+      }
+    },
+    quitarTarea(i){
+      this.$emit('quitarTarea', i)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  body{
+    margin: 0;
+    padding: 0;
+  }
+
+  .centro{
+    text-align: center;
+  }
+
+  .lista{
+    text-align: left;
+    margin-left: 40vw;
+  }
+</style>
